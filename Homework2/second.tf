c0de40199e1411f527b28c6683817c2886591849
dvdrbn <david.rvb@gmail.com>
2020-10-24 19:15:49 +0300

    base

---
+++ Homework2/second.tf
@@ -0,0 +1,100 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  region     = var.region
+}
+
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ami" "ubuntu_1804" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+#This uses the default VPC.  It WILL NOT delete it on destroy.
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "nginx_sg"
+  description = "Allow ports for nginx"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                    = data.aws_ami.ubuntu_1804.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  count = 1
+
+  user_data = <<-EOF
+          #! /bin/bash
+          sudo apt-get install nginx -y -q
+          EOF
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+  }
+
+  tags = {
+    Name = "homework2-nginx-${count.index}"
+    Owner = "David"
+    purpose = "Learning"
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx[*].public_dns
+}
